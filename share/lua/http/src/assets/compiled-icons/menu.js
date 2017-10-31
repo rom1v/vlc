358420ed030d14b949fa022b8e82e7bdf9a01507
Gautam Chitnis <chitnis.gautam1@gmail.com>
2017-10-31 15:31:48 +0100

    http: add assets

---
+++ share/lua/http/src/assets/compiled-icons/menu.js
@@ -0,0 +1,10 @@
+import { svgIcon } from '../../components/svg-icon/svg-icon.component.js';
+
+svgIcon.register({
+  'menu': {
+    width: 24,
+    height: 24,
+    viewBox: '0 0 24.00 24.00',
+    data: `<path pid="0" d="M3 6h18v2H3V6zm0 5h18v2H3v-2zm0 5h18v2H3v-2z"/>`
+  }
+})
---
+++ share/lua/http/src/assets/compiled-icons/plus.js
@@ -0,0 +1,10 @@
+import { svgIcon } from '../../components/svg-icon/svg-icon.component.js';
+
+svgIcon.register({
+  'plus': {
+    width: 24,
+    height: 24,
+    viewBox: '0 0 24.00 24.00',
+    data: `<path pid="0" d="M19 12.998h-6v6h-2v-6H5v-2h6v-6h2v6h6v2z"/>`
+  }
+})
