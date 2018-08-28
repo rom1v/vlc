6f4216c58e96ed6bf55ddef43be8ae1caada9ace
Romain Vimont <rom1v@videolabs.io>
2018-08-28 22:54:37 +0200

    vlc_vector: add helpers for vectors
    Add a new API for handling general-purpose vectors, intended to replace
    ARRAY_*.
    
    Like ARRAY_*, it provides macros to handle a dynamic array generic
    over the type of its items.
    
    Contrary to ARRAY_*:
     - it does not abort on allocation failure (but reports the error);
     - it uses size_t instead of int to store the capacity and the size;
     - it checks for overflows on reallocation.
    
    For illustration purpose, embedding a vector of input_item_t* in a
    struct looks like:
    
        struct playlist {
            struct VLC_VECTOR(input_item_t *) items;
            // ...
        };
    
    The main features (with names inspired by std::vector from C++ and
    std::vec::Vec from Rust) are:
     - void vlc_vector_init(pv)
          init the vector (use VLC_VECTOR_INITIALIZER for static init)
     - void vlc_vector_clear(pv)
          clear the vector, and release any associated resources
     - vec.size
          read the size of the vector
     - vec.data[i]
          access the i_th item
     - bool vlc_vector_push(pv, item)
          push an item at the end of the vector
     - bool vlc_vector_insert(pv, index, item)
          insert an item at index
     - bool vlc_vector_insert_all(pv, index, items, count)
          insert several items at index
     - void vlc_vector_remove(pv, index)
          remove an item
     - void vlc_vector_remove_slice(pv, index, count)
          remove a slice of items
     - void vlc_vector_swap_remove(pv, index)
          remove an item in O(1) without preserving ordering
     - bool vlc_vector_reserve(pv, mincap)
          increase the capacity of the vector in advance
     - void vlc_vector_shrink_to_fit(pv)
          resize the vector to its actual size
     - void vlc_vector_index_of(pv, index, pidx)
          find the index of an item (returns the result in *pidx)
     - vlc_vector_foreach(item, pv)
          a for-each loop
    
    It uses an exponential growth policy for both increasing and decreasing
    the size. As a consequence, the amortized complexity of
    vlc_vector_push() is O(1).

---
+++ include/vlc_vector.h
@@ -0,0 +1,528 @@
+/******************************************************************************
+ * vlc_vector.h
+ ******************************************************************************
+ * Copyright © 2018 VLC authors and VideoLAN
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+#ifndef VLC_VECTOR_H
+#define VLC_VECTOR_H
+
+#include <stdbool.h>
+
+/**
+ * \defgroup vector Vector
+ * \ingroup cext
+ * @{
+ * \file
+ * This provides convenience helpers for vectors.
+ */
+
+/**
+ * Vector struct body.
+ *
+ * A vector is a dynamic array, managed by the vlc_vector_* helpers.
+ *
+ * It is generic over the type of its items, so it is implemented as macros.
+ *
+ * To use a vector, a new type must be defined:
+ *
+ * \verbatim
+ * struct vec_int VLC_VECTOR(int);
+ * \endverbatim
+ *
+ * The struct may be anonymous:
+ *
+ * \verbatim
+ * struct VLC_VECTOR(const char *) names;
+ * \endverbatim
+ *
+ * It is convenient to define a typedef to an anonymous structure:
+ *
+ * \verbatim
+ * typedef struct VLC_VECTOR(int) vec_int_t;
+ * \endverbatim
+ *
+ * Vector size is accessible via `vec.size`, and items are intended to be
+ * accessed directly, via `vec.data[i]`.
+ *
+ * Functions and macros having name ending with '_' are private.
+ */
+#define VLC_VECTOR(type) \
+{ \
+    size_t cap; \
+    size_t size; \
+    type *data; \
+}
+
+/**
+ * Static initializer for a vector.
+ */
+#define VLC_VECTOR_INITIALIZER { 0, 0, NULL }
+
+/**
+ * Initialize an empty vector.
+ */
+#define vlc_vector_init(pv) (void) \
+( \
+    /* cannot be implemened as do-while(0), called from vlc_vector_clear() */ \
+    (pv)->cap = 0, \
+    (pv)->size = 0, \
+    (pv)->data = NULL \
+)
+
+/**
+ * Clear a vector, and release any associated resources.
+ */
+#define vlc_vector_clear(pv) \
+( \
+    /* cannot be implemened as do-while(0), called from vlc_vector_resize() */ \
+    free((pv)->data), \
+    vlc_vector_init(pv) \
+)
+
+/**
+ * The minimal allocation size, in number of items.
+ *
+ * Private.
+ */
+#define VLC_VECTOR_MINCAP_ ((size_t) 10)
+
+static inline size_t
+vlc_vector_min_(size_t a, size_t b)
+{
+    return a < b ? a : b;
+}
+
+static inline size_t
+vlc_vector_max_(size_t a, size_t b)
+{
+    return a > b ? a : b;
+}
+
+static inline size_t
+vlc_vector_between_(size_t x, size_t min, size_t max)
+{
+    return vlc_vector_max_(min, vlc_vector_min_(max, x));
+}
+
+static inline size_t
+vlc_vector_enforce_size_t_(size_t value)
+{
+    return value;
+}
+
+#define VLC_VECTOR_FAILFLAG_ (~(((size_t) -1) >> 1)) /* only the MSB */
+
+/**
+ * Realloc data and update vector fields.
+ *
+ * On reallocation success, return the reallocated array and update the vector
+ * capacity and size.
+ *
+ * On reallocation failure, return `ptr`, keep `*psize` untouched, and set the
+ * failflag in `*pcap` to indicate allocation failure (to be consumed by
+ * `vlc_vector_test_and_reset_failflag_()`).
+ *
+ * This weird behavior allows to simultaneously:
+ *  - not require compiler extensions like "statement expressions"
+ *  - keep the vector data, size and capacity unchanged on reallocation failure
+ *  - not require output variables other than vector fields from the caller
+ *  - not violate the strict aliasing rules
+ *  - report the reallocation status (success or failure)
+ *
+ * Private.
+ *
+ * \param ptr the current data to realloc
+ * \param the requested capacity, in number of items
+ * \param the size of one item
+ * \pcap a pointer to the `cap` field of the vector [IN/OUT]
+ * \pcap a pointer to the `size` field of the vector [IN/OUT]
+ * \return the reallocated array, or `ptr` if reallocation failed
+ */
+static inline void *
+vlc_vector_reallocdata_(void *ptr, size_t count, size_t size,
+                        size_t *pcap, size_t *psize)
+{
+    void *n = vlc_reallocarray(ptr, count, size);
+    if (!n)
+    {
+        /* this vector implementation guarantees that the capacity may not
+         * exceed SIZE_MAX/2 (to prevent overflows), so we can use the MSB to
+         * report allocation failure */
+        *pcap |= VLC_VECTOR_FAILFLAG_;
+        return ptr;
+    }
+    *pcap = count;
+    *psize = vlc_vector_min_(*psize, count);
+    return n;
+}
+
+/**
+ * Test and reset the fail flag.
+ *
+ * \retval true if the flag was set
+ * \retval false if the flag was not set
+ */
+static inline bool
+vlc_vector_test_and_reset_failflag_(size_t *pcap)
+{
+    if (*pcap & VLC_VECTOR_FAILFLAG_)
+    {
+        *pcap &= ~VLC_VECTOR_FAILFLAG_;
+        return true;
+    }
+    return false;
+}
+
+/**
+ * Realloc the underlying array to `newcap`.
+ *
+ * Private.
+ *
+ * \param pv a pointer to the vector
+ * \param newcap (size_t) the requested size
+ * \retval true if no allocation failed
+ * \retval false on allocation failure (the vector is left untouched)
+ */
+#define vlc_vector_realloc_(pv, newcap) \
+( \
+    (pv)->data = vlc_vector_reallocdata_((pv)->data, newcap, \
+                                         sizeof(*(pv)->data), \
+                                         &(pv)->cap, &(pv)->size), \
+    !vlc_vector_test_and_reset_failflag_(&(pv)->cap) \
+)
+
+/**
+ * Resize the vector to `newcap` exactly.
+ *
+ * If `newcap` is 0, the vector is cleared.
+ *
+ * \param pv a pointer to the vector
+ * \param newcap (size_t) the requested capacity
+ * \retval true if no allocation failed
+ * \retval false on allocation failure (the vector is left untouched)
+ */
+#define vlc_vector_resize(pv, newcap) \
+( \
+    (pv)->cap == (newcap) /* nothing to do */ || \
+    ( \
+        (newcap) > 0 ? vlc_vector_realloc_(pv, newcap) \
+                     : (vlc_vector_clear(pv), true) \
+    ) \
+)
+
+static inline size_t
+vlc_vector_growsize_(size_t value)
+{
+    /* integer multiplication by 1.5 */
+    return value + (value >> 1);
+}
+
+/* SIZE_MAX/2 to fit in ssize_t, and so that cap*1.5 does not overflow. */
+#define vlc_vector_max_cap_(pv) (SIZE_MAX / 2 / sizeof(*(pv)->data))
+
+/**
+ * Increase the capacity of the vector to at least `mincap`.
+ *
+ * \param pv a pointer to the vector
+ * \param mincap (size_t) the requested capacity
+ * \retval true if no allocation failed
+ * \retval false on allocation failure (the vector is left untouched)
+ */
+#define vlc_vector_reserve(pv, mincap) \
+    /* avoid to allocate tiny arrays (< VLC_VECTOR_MINCAP_) */ \
+    vlc_vector_reserve_internal_(pv, \
+                                 vlc_vector_max_(mincap, VLC_VECTOR_MINCAP_))
+
+#define vlc_vector_reserve_internal_(pv, mincap) \
+( \
+    (mincap) <= (pv)->cap /* nothing to do */ || \
+    ( \
+        (mincap) <= vlc_vector_max_cap_(pv) /* not too big */ && \
+        vlc_vector_realloc_(pv, \
+                        /* multiply by 1.5, force between [mincap, maxcap] */ \
+                        vlc_vector_between_(vlc_vector_growsize_((pv)->cap), \
+                                            mincap, \
+                                            vlc_vector_max_cap_(pv))) \
+    ) \
+)
+
+/**
+ * Resize the vector so that its capacity equals its actual size.
+ *
+ * \param pv a pointer to the vector
+ */
+#define vlc_vector_shrink_to_fit(pv) \
+    (void) /* decreasing the size may not fail */ \
+    vlc_vector_resize(pv, (pv)->size)
+
+/**
+ * Resize the vector down automatically.
+ *
+ * Shrink only when necessary (in practice when cap > (size+5)*1.5)
+ *
+ * \param pv a pointer to the vector
+ */
+#define vlc_vector_autoshrink(pv) (void) \
+( \
+    (pv)->cap <= VLC_VECTOR_MINCAP_ /* do not shrink to tiny length */ || \
+    (pv)->cap < vlc_vector_growsize_((pv)->size+5) /* no need to shrink */ || \
+    vlc_vector_resize(pv, vlc_vector_max_((pv)->size+5, VLC_VECTOR_MINCAP_)) \
+)
+
+/**
+ * Push an item at the end of the vector.
+ *
+ * The amortized complexity is O(1).
+ *
+ * \param pv a pointer to the vector
+ * \param item the item to append
+ * \retval true if no allocation failed
+ * \retval false on allocation failure (the vector is left untouched)
+ */
+#define vlc_vector_push(pv, item) \
+( \
+    vlc_vector_reserve(pv, (pv)->size + 1) && \
+    ( \
+        (pv)->data[(pv)->size++] = (item), \
+        true \
+    ) \
+)
+
+/**
+ * Append `count` items at the end of the vector.
+ *
+ * \param pv a pointer to the vector
+ * \param items the items array to append
+ * \param count the number of items in the array
+ * \retval true if no allocation failed
+ * \retval false on allocation failure (the vector is left untouched)
+ */
+#define vlc_vector_push_all(pv, items, count) \
+    vlc_vector_push_all_internal_(pv, items, vlc_vector_enforce_size_t_(count))
+
+#define vlc_vector_push_all_internal_(pv, items, count) \
+( \
+    vlc_vector_check_same_ptr_type_((pv)->data, items), \
+    vlc_vector_reserve(pv, (pv)->size + (count)) && \
+    ( \
+        memcpy(&(pv)->data[(pv)->size], items, (count) * sizeof(*(pv)->data)), \
+        (pv)->size += (count), \
+        true \
+    ) \
+)
+
+/**
+ * Insert an item at the given index.
+ *
+ * The items in range [index; size-1] will be moved.
+ *
+ * \param pv a pointer to the vector
+ * \param index the index where the item is to be inserted
+ * \param item the item to append
+ * \retval true if no allocation failed
+ * \retval false on allocation failure (the vector is left untouched)
+ */
+#define vlc_vector_insert(pv, index, item) \
+    vlc_vector_insert_internal_(pv, vlc_vector_enforce_size_t_(index), item)
+
+#define vlc_vector_insert_internal_(pv, index, item) \
+( \
+    vlc_vector_reserve(pv, (pv)->size + 1) && \
+    ( \
+        (index) == (pv)->size || \
+        ( \
+            memmove(&(pv)->data[(index) + 1], \
+                    &(pv)->data[index], \
+                    ((pv)->size - (index)) * sizeof(*(pv)->data)), \
+            true \
+        ) \
+    ) && \
+    ( \
+        (pv)->data[index] = (item), \
+        (pv)->size++, \
+        true \
+    ) \
+)
+
+/**
+ * Insert `count` items at the given index.
+ *
+ * The items in range [index; size-1] will be moved.
+ *
+ * \param pv a pointer to the vector
+ * \param index the index where the items are to be inserted
+ * \param items the items array to append
+ * \param count the number of items in the array
+ * \retval true if no allocation failed
+ * \retval false on allocation failure (the vector is left untouched)
+ */
+#define vlc_vector_insert_all(pv, index, items, count) \
+    vlc_vector_insert_all_internal_(pv, vlc_vector_enforce_size_t_(index), \
+                                    items, vlc_vector_enforce_size_t_(count))
+
+#define vlc_vector_check_same_ptr_type_(a, b) \
+    (void) ((a) == (b)) /* warn on type mismatch */
+
+#define vlc_vector_insert_all_internal_(pv, index, items, count) \
+( \
+    vlc_vector_check_same_ptr_type_((pv)->data, items), \
+    vlc_vector_reserve(pv, (pv)->size + (count)) && \
+    ( \
+        (index) == (pv)->size || \
+        ( \
+            memmove(&(pv)->data[(index) + (count)], \
+                    &(pv)->data[index], \
+                    ((pv)->size - (index)) * sizeof(*(pv)->data)), \
+            true \
+        ) \
+    ) && \
+    ( \
+        memcpy(&(pv)->data[index], items, (count) * sizeof(*(pv)->data)), \
+        (pv)->size += (count), \
+        true \
+    ) \
+)
+
+/**
+ * Remove a slice of items, without shrinking the array.
+ *
+ * If you have no good reason to use the _noshrink() version, use
+ * vlc_vector_remove_slice() instead.
+ *
+ * The items in range [index+count; size-1] will be moved.
+ *
+ * \param pv a pointer to the vector
+ * \param index the index of the first item to remove
+ * \param count the number of items to remove
+ */
+#define vlc_vector_remove_slice_noshrink(pv, index, count) \
+    vlc_vector_remove_slice_noshrink_internal_(pv, \
+            vlc_vector_enforce_size_t_(index), \
+            vlc_vector_enforce_size_t_(count))
+
+#define vlc_vector_remove_slice_noshrink_internal_(pv, index, count) \
+    do { \
+        if ((index) + (count) < (pv)->size) \
+            memmove(&(pv)->data[index], \
+                    &(pv)->data[(index) + (count)], \
+                    ((pv)->size - (index) - (count)) * sizeof(*(pv)->data)); \
+        (pv)->size -= (count); \
+    } while (0)
+
+/**
+ * Remove a slice of items.
+ *
+ * The items in range [index+count; size-1] will be moved.
+ *
+ * \param pv a pointer to the vector
+ * \param index the index of the first item to remove
+ * \param count the number of items to remove
+ */
+#define vlc_vector_remove_slice(pv, index, count) \
+    do { \
+        vlc_vector_remove_slice_noshrink(pv, index, count); \
+        vlc_vector_autoshrink(pv); \
+    } while (0)
+
+/**
+ * Remove an item, without shrinking the array.
+ *
+ * If you have no good reason to use the _noshrink() version, use
+ * vlc_vector_remove() instead.
+ *
+ * The items in range [index+1; size-1] will be moved.
+ *
+ * \param pv a pointer to the vector
+ * \param index the index of item to remove
+ */
+#define vlc_vector_remove_noshrink(pv, index) \
+    vlc_vector_remove_slice_noshrink(pv, index, 1)
+
+/**
+ * Remove an item.
+ *
+ * The items in range [index+1; size-1] will be moved.
+ *
+ * \param pv a pointer to the vector
+ * \param index the index of item to remove
+ */
+#define vlc_vector_remove(pv, index) \
+    do { \
+        vlc_vector_remove_noshrink(pv, index); \
+        vlc_vector_autoshrink(pv); \
+    } while (0)
+
+/**
+ * Remove an item.
+ *
+ * The removed item is replaced by the last item of the vector.
+ *
+ * This does not preserve ordering, but is O(1). This is useful when the order
+ * of items is not meaningful.
+ *
+ * \param pv a pointer to the vector
+ * \param index the index of item to remove
+ */
+#define vlc_vector_swap_remove(pv, index) \
+    (pv)->data[index] = (pv)->data[--(pv)->size]
+
+/**
+ * Return the index of an item.
+ *
+ * Iterate over all items to find a given item.
+ *
+ * Use only for vectors of primitive types or pointers.
+ *
+ * The result is written to `*(pidx)`:
+ *  - the index of the item if it is found;
+ *  - -1 if it is not found.
+ *
+ * \param pv a pointer to the vector
+ * \param item the item to find (compared with ==)
+ * \param a pointer to the result (ssize_t *) [OUT]
+ */
+#define vlc_vector_index_of(pv, item, pidx) \
+    do { \
+        ssize_t *out = pidx; /* warning/error on type mismatch */ \
+        size_t vlc_vector_find_idx_; \
+        for (vlc_vector_find_idx_ = 0; \
+             vlc_vector_find_idx_ < (pv)->size; \
+             ++vlc_vector_find_idx_) \
+            if ((pv)->data[vlc_vector_find_idx_] == (item)) \
+                break; \
+        *out = vlc_vector_find_idx_ == (pv)->size ? -1 : \
+            (ssize_t) vlc_vector_find_idx_; \
+    } while (0)
+
+/**
+ * For-each loop.
+ *
+ * Use only for vectors of primitive types or pointers (every struct would be
+ * copied for a vector of structs).
+ *
+ * \param item the iteration variable [OUT]
+ * \param pv a pointer to the vector [OUT]
+ */
+#define vlc_vector_foreach(item, pv) \
+    for (size_t vlc_vector_idx_##item = 0; \
+         vlc_vector_idx_##item < (pv)->size && \
+             ((item) = (pv)->data[vlc_vector_idx_##item], true); \
+         ++vlc_vector_idx_##item)
+
+/** @} */
+
+#endif
