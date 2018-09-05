be8724bc233e5f6f28f23c479968d84aaa04a1c3
Romain Vimont <rom1v@videolabs.io>
2018-09-05 17:25:38 +0200

    Make Playlist independant of PlaylistModel

--- modules/gui/qt/components/playlist_new/playlist_model.cpp
+++ modules/gui/qt/components/playlist_new/playlist_model.cpp
@@ -4,28 +4,20 @@
 
 #include "playlist_model.hpp"
 
-PlaylistModel::PlaylistModel(vlc_playlist_t *rawPlaylist, QObject *parent)
+PlaylistModel::PlaylistModel(Playlist *playlist, QObject *parent)
     : QAbstractListModel(parent)
-    , playlist(rawPlaylist)
+    , playlist(playlist)
 {
-    connect(&playlist, &Playlist::playlistCleared,
+    connect(playlist, &Playlist::playlistCleared,
             this, &PlaylistModel::onPlaylistCleared);
-    connect(&playlist, &Playlist::playlistItemsAdded,
+    connect(playlist, &Playlist::playlistItemsAdded,
             this, &PlaylistModel::onPlaylistItemsAdded);
-    connect(&playlist, &Playlist::playlistItemsRemoved,
+    connect(playlist, &Playlist::playlistItemsRemoved,
             this, &PlaylistModel::onPlaylistItemsRemoved);
-    connect(&playlist, &Playlist::playlistItemUpdated,
+    connect(playlist, &Playlist::playlistItemUpdated,
             this, &PlaylistModel::onPlaylistItemUpdated);
-    connect(&playlist, &Playlist::playlistPlaybackRepeatChanged,
-            this, &PlaylistModel::onPlaylistPlaybackRepeatChanged);
-    connect(&playlist, &Playlist::playlistPlaybackOrderChanged,
-            this, &PlaylistModel::onPlaylistPlaybackOrderChanged);
-    connect(&playlist, &Playlist::playlistCurrentItemChanged,
+    connect(playlist, &Playlist::playlistCurrentItemChanged,
             this, &PlaylistModel::onPlaylistCurrentItemChanged);
-    connect(&playlist, &Playlist::playlistHasNextChanged,
-            this, &PlaylistModel::onPlaylistHasNextChanged);
-    connect(&playlist, &Playlist::playlistHasPrevChanged,
-            this, &PlaylistModel::onPlaylistHasPrevChanged);
 }
 
 void
@@ -53,33 +45,7 @@ PlaylistModel::onPlaylistItemUpdated(size_t index, PlaylistItem item)
 }
 
 void
-PlaylistModel::onPlaylistPlaybackRepeatChanged(
-                                    enum vlc_playlist_playback_repeat repeat)
-{
-
-}
-
-void
-PlaylistModel::onPlaylistPlaybackOrderChanged(
-                                    enum vlc_playlist_playback_order order)
-{
-
-}
-
-void
 PlaylistModel::onPlaylistCurrentItemChanged(ssize_t index, PlaylistItem)
-{
-
-}
-
-void
-PlaylistModel::onPlaylistHasNextChanged(bool hasNext)
-{
-
-}
-
-void
-PlaylistModel::onPlaylistHasPrevChanged(bool hasPrev)
 {
 
 }
