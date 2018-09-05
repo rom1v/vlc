fee3cbc872bb308e743c3509b449fdda72330049
Romain Vimont <rom1v@videolabs.io>
2018-09-05 17:25:38 +0200

    Make Playlist independant of PlaylistModel

--- modules/gui/qt/components/playlist_new/playlist_model.hpp
+++ modules/gui/qt/components/playlist_new/playlist_model.hpp
@@ -20,7 +20,7 @@ public:
         TitleRole = Qt::UserRole,
     };
 
-    PlaylistModel(vlc_playlist_t *, QObject *parent = nullptr);
+    PlaylistModel(Playlist *playlist, QObject *parent = nullptr);
 
     QHash<int, QByteArray> roleNames() const override;
     int rowCount(const QModelIndex &parent = {}) const override;
@@ -32,14 +32,10 @@ private slots:
     void onPlaylistItemsAdded(size_t index, QVector<PlaylistItem>);
     void onPlaylistItemsRemoved(size_t index, QVector<PlaylistItem>);
     void onPlaylistItemUpdated(size_t index, PlaylistItem);
-    void onPlaylistPlaybackRepeatChanged(enum vlc_playlist_playback_repeat);
-    void onPlaylistPlaybackOrderChanged(enum vlc_playlist_playback_order);
     void onPlaylistCurrentItemChanged(ssize_t index, PlaylistItem);
-    void onPlaylistHasNextChanged(bool hasNext);
-    void onPlaylistHasPrevChanged(bool hasPrev);
 
 private:
-    Playlist playlist;
+    Playlist *playlist;
     QVector<PlaylistItem> items; /* access only from the UI thread */
 };
 
