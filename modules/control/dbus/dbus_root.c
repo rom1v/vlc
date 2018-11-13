a4e5260d352fa295d813f5dd7f01f09fa221fdc0
Victorien Le Couviour--Tuffet <victorien.lecouviour.tuffet@gmail.com>
2018-11-13 19:50:18 +0100

    dbus: use new playlist / player

--- modules/control/dbus/dbus_root.c
+++ modules/control/dbus/dbus_root.c
@@ -31,10 +31,8 @@
 
 #include <vlc_common.h>
 #include <vlc_interface.h>
-#include <vlc_input.h>
 #include <vlc_vout.h>
 #include <vlc_plugin.h>
-#include <vlc_playlist_legacy.h>
 
 #include <unistd.h>
 #include <limits.h>
@@ -87,26 +85,10 @@ MarshalIdentity( intf_thread_t *p_intf, DBusMessageIter *container )
 
 static int
 MarshalCanSetFullscreen( intf_thread_t *p_intf, DBusMessageIter *container )
-{
-    input_thread_t *p_input = NULL;
-    dbus_bool_t     b_ret   = FALSE;
-
-    if (p_intf->p_sys->p_input)
-    {
-        p_input = (input_thread_t*) vlc_object_hold( p_intf->p_sys->p_input );
-        vout_thread_t* p_vout = input_GetVout( p_input );
-        vlc_object_release( p_input );
-
-        if ( p_vout )
-        {
-            b_ret = TRUE;
-            vlc_object_release( p_vout );
-        }
-    }
-
+{ VLC_UNUSED(p_intf);
+    dbus_bool_t b_ret = TRUE;
     if (!dbus_message_iter_append_basic( container, DBUS_TYPE_BOOLEAN, &b_ret ))
         return VLC_ENOMEM;
-
     return VLC_SUCCESS;
 }
 
@@ -114,16 +96,11 @@ static int
 MarshalFullscreen( intf_thread_t *p_intf, DBusMessageIter *container )
 {
     dbus_bool_t b_fullscreen;
-
-    if ( p_intf->p_sys->p_playlist )
-        b_fullscreen = var_GetBool( p_intf->p_sys->p_playlist , "fullscreen" );
-    else
-        b_fullscreen = FALSE;
-
+    vlc_player_t *player = vlc_playlist_GetPlayer(p_intf->p_sys->playlist);
+    b_fullscreen = vlc_player_vout_IsFullscreen(player);
     if (!dbus_message_iter_append_basic( container,
             DBUS_TYPE_BOOLEAN, &b_fullscreen ))
         return VLC_ENOMEM;
-
     return VLC_SUCCESS;
 }
 
@@ -131,22 +108,12 @@ DBUS_METHOD( FullscreenSet )
 {
     REPLY_INIT;
     dbus_bool_t b_fullscreen;
-    input_thread_t *p_input = NULL;
 
     if( VLC_SUCCESS != DemarshalSetPropertyValue( p_from, &b_fullscreen ) )
         return DBUS_HANDLER_RESULT_NOT_YET_HANDLED;
 
-    if (INTF->p_sys->p_input)
-    {
-        p_input = (input_thread_t*) vlc_object_hold( INTF->p_sys->p_input );
-        vout_thread_t* p_vout = input_GetVout( p_input );
-        vlc_object_release( p_input );
-
-        if ( p_vout )
-            var_SetBool( p_vout, "fullscreen", ( b_fullscreen == TRUE ) );
-        if ( PL )
-            var_SetBool( PL , "fullscreen", ( b_fullscreen == TRUE ) );
-    }
+    vlc_player_t *player = vlc_playlist_GetPlayer(PL);
+    vlc_player_vout_SetFullscreen(player, b_fullscreen);
 
     REPLY_SEND;
 }
@@ -267,7 +234,7 @@ DBUS_METHOD( Quit )
 DBUS_METHOD( Raise )
 {/* shows vlc's main window */
     REPLY_INIT;
-    var_TriggerCallback( pl_Get(INTF), "intf-show" );
+    var_TriggerCallback(INTF->obj.libvlc, "intf-show" );
     REPLY_SEND;
 }
 
