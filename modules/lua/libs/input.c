67b1e619d15d5f7d0b036352506b2df0a8655dd0
Romain Vimont <rom1v@videolabs.io>
2019-01-29 17:12:20 +0100

    lua: expose player titles and chapters
    Titles and chapters were handled from LUA via VLC variables. Expose
    functions from the new player instead.

--- modules/lua/libs/input.c
+++ modules/lua/libs/input.c
@@ -100,6 +100,129 @@ static int vlclua_input_is_playing( lua_State *L )
     return 1;
 }
 
+static int vlclua_player_get_title_index(lua_State *L)
+{
+    vlc_player_t *player = vlclua_get_player_internal(L);
+
+    vlc_player_Lock(player);
+    ssize_t idx = vlc_player_GetSelectedTitleIdx(player);
+    vlc_player_Unlock(player);
+
+    lua_pushinteger(L, idx);
+    return 1;
+}
+
+static int vlclua_player_get_titles_count(lua_State *L)
+{
+    vlc_player_t *player = vlclua_get_player_internal(L);
+
+    vlc_player_Lock(player);
+    const struct vlc_player_title_list *titles =
+            vlc_player_GetTitleList(player);
+    size_t count = titles ? vlc_player_title_list_GetCount(titles) : 0;
+    vlc_player_Unlock(player);
+
+    lua_pushinteger(L, count);
+    return 1;
+}
+
+static int vlclua_player_title_next(lua_State *L)
+{
+    vlc_player_t *player = vlclua_get_player_internal(L);
+
+    vlc_player_Lock(player);
+    vlc_player_SelectNextTitle(player);
+    vlc_player_Unlock(player);
+
+    return 0;
+}
+
+static int vlclua_player_title_prev(lua_State *L)
+{
+    vlc_player_t *player = vlclua_get_player_internal(L);
+
+    vlc_player_Lock(player);
+    vlc_player_SelectPrevTitle(player);
+    vlc_player_Unlock(player);
+
+    return 0;
+}
+
+static int vlclua_player_title_goto(lua_State *L)
+{
+    int idx = luaL_checkinteger(L, 1);
+
+    vlc_player_t *player = vlclua_get_player_internal(L);
+
+    vlc_player_Lock(player);
+    vlc_player_SelectTitleIdx(player, idx);
+    vlc_player_Unlock(player);
+
+    return 0;
+}
+
+static int vlclua_player_get_chapter_index(lua_State *L)
+{
+    vlc_player_t *player = vlclua_get_player_internal(L);
+
+    vlc_player_Lock(player);
+    ssize_t idx = vlc_player_GetSelectedChapterIdx(player);
+    vlc_player_Unlock(player);
+
+    lua_pushinteger(L, idx);
+    return 1;
+}
+
+static int vlclua_player_get_chapters_count(lua_State *L)
+{
+    vlc_player_t *player = vlclua_get_player_internal(L);
+
+    vlc_player_Lock(player);
+    const struct vlc_player_title *current_title =
+        vlc_player_GetSelectedTitle(player);
+
+    size_t count = current_title ? current_title->chapter_count : 0;
+    vlc_player_Unlock(player);
+
+    lua_pushinteger(L, count);
+    return 1;
+}
+
+static int vlclua_player_chapter_next(lua_State *L)
+{
+    vlc_player_t *player = vlclua_get_player_internal(L);
+
+    vlc_player_Lock(player);
+    vlc_player_SelectNextChapter(player);
+    vlc_player_Unlock(player);
+
+    return 0;
+}
+
+static int vlclua_player_chapter_prev(lua_State *L)
+{
+    vlc_player_t *player = vlclua_get_player_internal(L);
+
+    vlc_player_Lock(player);
+    vlc_player_SelectPrevChapter(player);
+    vlc_player_Unlock(player);
+
+    return 0;
+}
+
+static int vlclua_player_chapter_goto(lua_State *L)
+{
+    int idx = luaL_checkinteger(L, 1);
+
+    vlc_player_t *player = vlclua_get_player_internal(L);
+
+    vlc_player_Lock(player);
+    vlc_player_SelectChapterIdx(player, idx);
+    vlc_player_Unlock(player);
+
+    return 0;
+}
+
 static int vlclua_input_metas_internal( lua_State *L, input_item_t *p_item )
 {
     if( !p_item )
@@ -401,6 +524,16 @@ static const luaL_Reg vlclua_input_reg[] = {
     { "item", vlclua_input_item_get_current },
     { "add_subtitle", vlclua_input_add_subtitle_path },
     { "add_subtitle_mrl", vlclua_input_add_subtitle_mrl },
+    { "get_title_index", vlclua_player_get_title_index },
+    { "get_titles_count", vlclua_player_get_titles_count },
+    { "title_next", vlclua_player_title_next },
+    { "title_prev", vlclua_player_title_prev },
+    { "title_goto", vlclua_player_title_goto },
+    { "get_chapter_index", vlclua_player_get_chapter_index },
+    { "get_chapters_count", vlclua_player_get_chapters_count },
+    { "chapter_next", vlclua_player_chapter_next },
+    { "chapter_prev", vlclua_player_chapter_prev },
+    { "chapter_goto", vlclua_player_chapter_goto },
     { NULL, NULL }
 };
 
