4cc08b90c7136d7e3c6a8597d0d9ef02bd467e09
Alexandre Janniaux <ajanni@videolabs.io>
2019-08-26 15:07:54 +0200

    opengl: filter: default to non-blending info

--- modules/video_output/opengl/vout_helper.c
+++ modules/video_output/opengl/vout_helper.c
@@ -1841,7 +1841,7 @@ int vout_display_opengl_AppendFilter(vout_display_opengl_t *vgl,
     wrapper->texture_count = 0;
     wrapper->filter.fmt = &vgl->fmt; //< TODO: replace by fmt_in/fmt_out const pointer
     wrapper->filter.vt = &vgl->vt;
-    wrapper->filter.info.blend = true;
+    wrapper->filter.info.blend = false;
 
     /* Mutable format configuration for the filter input/output. */
     uint32_t chroma = prev_filter != NULL ? prev_filter->fmt_out.i_chroma
