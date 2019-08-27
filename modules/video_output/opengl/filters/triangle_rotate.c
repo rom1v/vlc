69415e66214356321629406f76dd8302d7ec3c88
Alexandre Janniaux <ajanni@videolabs.io>
2019-08-27 14:33:22 +0200

    opengl: triangle_rotate: set info.blend

--- modules/video_output/opengl/filters/triangle_rotate.c
+++ modules/video_output/opengl/filters/triangle_rotate.c
@@ -231,6 +231,8 @@ static int Open(struct vlc_gl_filter *filter,
 
     filter->filter = FilterInput;
     filter->close = FilterClose;
+    filter->info.blend = true;
+
     return VLC_SUCCESS;
 }
 
