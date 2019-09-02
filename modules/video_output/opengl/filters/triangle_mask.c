f867fb6ad66036504c8cc12cb69efb01f44359bb
Alexandre Janniaux <ajanni@videolabs.io>
2019-09-02 09:55:50 +0200

    opengl: triangle_mask: force output format

--- modules/video_output/opengl/filters/triangle_mask.c
+++ modules/video_output/opengl/filters/triangle_mask.c
@@ -176,6 +176,7 @@ static int Open(struct vlc_gl_filter *filter,
     filter->close = FilterClose;
 
     fmt_in->i_chroma = VLC_CODEC_RGBA;
+    fmt_out->i_chroma = VLC_CODEC_RGBA;
 
     return VLC_SUCCESS;
 }
