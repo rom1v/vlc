3e515600a28a28b0de41b97c8162294bc6a52e60
Romain Vimont <rom1v@videolabs.io>
2019-10-14 16:18:50 +0200

    wip

--- modules/video_output/opengl/importer.h
+++ modules/video_output/opengl/importer.h
@@ -28,7 +28,13 @@
 
 struct vlc_gl_importer;
 
+typedef int (*vlc_gl_importer_open_fn)(struct vlc_gl_importer *);
+
 struct vlc_gl_importer_ops {
+    int (*alloc_textures)(struct vlc_gl_importer *importer, GLuint textures[],
+                          const GLsizei tex_width[],
+                          const GLsizei tex_height[], unsigned tex_count);
+
     int (*import)(struct vlc_gl_importer *importer, GLuint textures[],
                   const GLsizei tex_width[], const GLsizei text_height[],
                   unsigned tex_count, picture_t *pic,
@@ -40,10 +46,15 @@ struct vlc_gl_importer_ops {
 // TODO move some fields into a "private" struct?
 struct vlc_gl_importer {
     struct vlc_object_t obj;
+    module_t *module;
 
-    const opengl_vtable_t *vt;
+    const opengl_vtable_t *gl;
     GLenum tex_target;
 
+    /* Pointer to decoder video context, set by the caller (can be NULL) */
+    vlc_video_context *vctx;
+
+    /* Initialized by the caller */
     video_format_t fmt;
 
     /* Initialized by the importer */
@@ -59,6 +70,7 @@ struct vlc_gl_importer {
         GLenum format;
         GLenum type;
     } cfg[PICTURE_PLANE_MAX];
+    unsigned tex_count;
 
     void *sys;
     const struct vlc_gl_importer_ops *ops;
--- modules/video_output/opengl/importers/sw.c
+++ modules/video_output/opengl/importers/sw.c
@@ -34,12 +34,54 @@ struct sw_sys {
 };
 
 static int
+get_tex_format_size(struct vlc_gl_importer *importer, int target,
+                    int tex_format, int tex_internal, int tex_type)
+{
+    const opengl_vtable_t *gl = importer->gl;
+    if (!gl->GetTexLevelParameteriv)
+        return -1;
+
+    GLint tex_param_size;
+    int mul = 1;
+    switch (tex_format)
+    {
+        case GL_BGRA:
+            mul = 4;
+            /* fall through */
+        case GL_RED:
+        case GL_RG:
+            tex_param_size = GL_TEXTURE_RED_SIZE;
+            break;
+        case GL_LUMINANCE:
+            tex_param_size = GL_TEXTURE_LUMINANCE_SIZE;
+            break;
+        default:
+            return -1;
+    }
+
+    GLuint texture;
+    gl->GenTextures(1, &texture);
+    gl->BindTexture(target, texture);
+    gl->TexImage2D(target, 0, tex_internal, 64, 64, 0, tex_format, tex_type,
+                   NULL);
+
+    GLint size = 0;
+    gl->GetTexLevelParameteriv(target, 0, tex_param_size, &size);
+
+    gl->DeleteTextures(1, &texture);
+
+    if (size > 0)
+        size *= mul;
+    return size;
+}
+
+static int
 upload_plane(struct vlc_gl_importer *importer, unsigned idx, GLsizei width,
              GLsizei height, unsigned pitch, unsigned visible_pitch,
              const void *pixels)
 {
     struct vlc_gl_tex_cfg *cfg = &importer->cfg[idx];
-    const opengl_vtable_t *gl = importer->vt;
+    const opengl_vtable_t *gl = importer->gl;
 
     /* This unpack alignment is the default, but setting it just in case. */
     gl->PixelStorei(GL_UNPACK_ALIGNMENT, 4);
@@ -54,11 +96,27 @@ upload_plane(struct vlc_gl_importer *importer, unsigned idx, GLsizei width,
 }
 
 static int
+AllocTextures(struct vlc_gl_importer *importer, GLuint textures[],
+              const GLsizei tex_width[], const GLsizei tex_height[],
+              unsigned tex_count)
+{
+    for (unsigned i = 0; i < tex_count; ++i)
+    {
+        const opengl_vtable_t *gl = importer->gl;
+        gl->BindTexture(importer->tex_target, textures[i]);
+        gl->TexImage2D(importer->tex_target, 0, importer->cfg[i].internal,
+                       tex_width[i], tex_height[i], 0, importer->cfg[i].format,
+                       importer->cfg[i].type, NULL);
+    }
+    return VLC_SUCCESS;
+}
+
+static int
 Import(struct vlc_gl_importer *importer, GLuint textures[],
        const GLsizei tex_width[], const GLsizei tex_height[],
        unsigned tex_count, picture_t *pic, const size_t plane_offsets[])
 {
-    const opengl_vtable_t *gl = importer->vt;
+    const opengl_vtable_t *gl = importer->gl;
 
     for (unsigned i = 0; i < tex_count; ++i)
     {
@@ -84,7 +142,95 @@ Close(struct vlc_gl_importer *importer)
     free(importer->sys);
 }
 
+static int
+fill_cfg_xyz12(struct vlc_gl_importer *importer)
+{
+    // TODO
+}
+
+static int
+fill_cfg_yuv(struct vlc_gl_importer *importer, vlc_fourcc_t chroma)
+{
+    // TODO
+    return VLC_EGENERIC;
+}
+
+static int
+fill_cfg_rgb(struct vlc_gl_importer *importer, vlc_fourcc_t chroma)
+{
+    switch (chroma)
+    {
+        case VLC_CODEC_RGB32:
+        case VLC_CODEC_RGBA:
+            importer->cfg[0] = (struct vlc_gl_tex_cfg) {
+                .w = { 1, 1 },
+                .h = { 1, 1 },
+                .internal = GL_RGBA,
+                .format = GL_RGBA,
+                .type = GL_UNSIGNED_BYTE,
+            };
+            break;
+        case VLC_CODEC_BGRA: {
+            if (get_tex_format_size(importer, importer->tex_target, GL_BGRA,
+                                    GL_RGBA, GL_UNSIGNED_BYTE) != 32)
+                return VLC_EGENERIC;
+            importer->cfg[0] = (struct vlc_gl_tex_cfg) {
+                .w = { 1, 1 },
+                .h = { 1, 1 },
+                .internal = GL_RGBA,
+                .format = GL_BGRA,
+                .type = GL_UNSIGNED_BYTE,
+            };
+            break;
+        }
+        default:
+            return VLC_EGENERIC;
+    }
+
+    importer->tex_count = 1;
+    return VLC_SUCCESS;
+}
+
+static int
+fill_cfg_for_chroma(struct vlc_gl_importer *importer, vlc_fourcc_t chroma)
+{
+    if (chroma == VLC_CODEC_XYZ12)
+        return fill_cfg_xyz12(importer);
+
+    if (vlc_fourcc_IsYUV(chroma))
+        return fill_cfg_yuv(importer, chroma);
+
+    return fill_cfg_rgb(importer, chroma);
+}
+
+static inline const vlc_fourcc_t *
+fallback_list(vlc_fourcc_t chroma)
+{
+    if (chroma == VLC_CODEC_XYZ12)
+    {
+        static const vlc_fourcc_t xyz12_list[] = { VLC_CODEC_XYZ12, 0 };
+        return xyz12_list;
+    }
+
+    return vlc_fourcc_GetFallback(chroma);
+}
+
+static int
+fill_cfg(struct vlc_gl_importer *importer)
+{
+    const vlc_fourcc_t *list = fallback_list(importer->fmt.i_chroma);
+
+    while (*list)
+    {
+        int ret = fill_cfg_for_chroma(importer, *list);
+        if (ret == VLC_SUCCESS)
+            return VLC_SUCCESS;
+    }
+    return VLC_EGENERIC;
+}
+
 static const struct vlc_gl_importer_ops ops = {
+    .alloc_textures = AllocTextures,
     .import = Import,
     .close = Close,
 };
@@ -98,10 +244,9 @@ Open(struct vlc_gl_importer *importer)
 
     importer->ops = &ops;
 
-    if (vlc_fourcc_IsYUV(importer->fmt.i_chroma))
-    {
-
-    }
+    int ret = fill_cfg(importer);
+    if (ret != VLC_SUCCESS)
+        return ret;
 
     return VLC_SUCCESS;
 }
