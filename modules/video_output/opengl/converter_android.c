9f198ae3522c49d8c3e4cecc253ce3ce3602ab0a
Romain Vimont <rom1v@videolabs.io>
2019-12-13 15:22:56 +0100

    adapt android converter WIP

--- modules/video_output/opengl/converter_android.c
+++ modules/video_output/opengl/converter_android.c
@@ -38,15 +38,15 @@ struct priv
 };
 
 static int
-tc_anop_allocate_textures(const opengl_tex_converter_t *tc, GLuint *textures,
+tc_anop_allocate_textures(const struct vlc_gl_importer *imp, GLuint *textures,
                           const GLsizei *tex_width, const GLsizei *tex_height)
 {
     (void) tex_width; (void) tex_height;
-    struct priv *priv = tc->priv;
+    struct priv *priv = imp->priv;
     assert(textures[0] != 0);
     if (SurfaceTexture_attachToGLContext(priv->awh, textures[0]) != 0)
     {
-        msg_Err(tc->gl, "SurfaceTexture_attachToGLContext failed");
+        msg_Err(imp->gl, "SurfaceTexture_attachToGLContext failed");
         return VLC_EGENERIC;
     }
     priv->stex_attached = true;
@@ -54,7 +54,7 @@ tc_anop_allocate_textures(const opengl_tex_converter_t *tc, GLuint *textures,
 }
 
 static int
-tc_anop_update(const opengl_tex_converter_t *tc, GLuint *textures,
+tc_anop_update(const struct vlc_gl_importer *imp, GLuint *textures,
                const GLsizei *tex_width, const GLsizei *tex_height,
                picture_t *pic, const size_t *plane_offset)
 {
@@ -65,7 +65,7 @@ tc_anop_update(const opengl_tex_converter_t *tc, GLuint *textures,
     if (plane_offset != NULL)
         return VLC_EGENERIC;
 
-    struct priv *priv = tc->priv;
+    struct priv *priv = imp->priv;
 
     if (!priv->avctx->render(pic->context))
         return VLC_SUCCESS; /* already rendered */
@@ -77,16 +77,16 @@ tc_anop_update(const opengl_tex_converter_t *tc, GLuint *textures,
         return VLC_EGENERIC;
     }
 
-    tc->vt->ActiveTexture(GL_TEXTURE0);
-    tc->vt->BindTexture(tc->tex_target, textures[0]);
+    imp->vt->ActiveTexture(GL_TEXTURE0);
+    imp->vt->BindTexture(imp->tex_target, textures[0]);
 
     return VLC_SUCCESS;
 }
 
 static const float *
-tc_get_transform_matrix(const opengl_tex_converter_t *tc)
+tc_get_transform_matrix(const struct vlc_gl_importer *imp)
 {
-    struct priv *priv = tc->priv;
+    struct priv *priv = imp->priv;
     return priv->transform_mtx;
 }
 
@@ -106,60 +106,64 @@ static int
 Open(vlc_object_t *obj)
 {
     opengl_tex_converter_t *tc = (void *) obj;
+    struct vlc_gl_importer *imp = &tc->importer;
 
-    if (tc->fmt.i_chroma != VLC_CODEC_ANDROID_OPAQUE
-     || !tc->gl->surface->handle.anativewindow
-     || !tc->vctx)
+    if (imp->fmt.i_chroma != VLC_CODEC_ANDROID_OPAQUE
+     || !imp->gl->surface->handle.anativewindow
+     || !imp->vctx)
         return VLC_EGENERIC;
 
     android_video_context_t *avctx =
-        vlc_video_context_GetPrivate(tc->vctx, VLC_VIDEO_CONTEXT_AWINDOW);
+        vlc_video_context_GetPrivate(imp->vctx, VLC_VIDEO_CONTEXT_AWINDOW);
 
     if (avctx->id != AWindow_SurfaceTexture)
         return VLC_EGENERIC;
 
-    tc->priv = malloc(sizeof(struct priv));
-    if (unlikely(tc->priv == NULL))
+    imp->priv = malloc(sizeof(struct priv));
+    if (unlikely(imp->priv == NULL))
         return VLC_ENOMEM;
 
-    struct priv *priv = tc->priv;
+    struct priv *priv = imp->priv;
     priv->avctx = avctx;
-    priv->awh = tc->gl->surface->handle.anativewindow;
+    priv->awh = imp->gl->surface->handle.anativewindow;
     priv->transform_mtx = NULL;
     priv->stex_attached = false;
 
-    tc->pf_allocate_textures = tc_anop_allocate_textures;
-    tc->pf_update         = tc_anop_update;
-    tc->pf_get_transform_matrix = tc_get_transform_matrix;
+    static const struct vlc_gl_importer_ops ops = {
+        .allocate_textures = tc_anop_allocate_textures,
+        .update_textures = tc_anop_update,
+        .get_transform_matrix = tc_get_transform_matrix,
+    };
+    imp->ops = &ops;
 
     /* The transform Matrix (uSTMatrix) given by the SurfaceTexture is not
      * using the same origin than us. Ask the caller to rotate textures
      * coordinates, via the vertex shader, by forcing an orientation. */
-    switch (tc->fmt.orientation)
+    switch (imp->fmt.orientation)
     {
         case ORIENT_TOP_LEFT:
-            tc->fmt.orientation = ORIENT_BOTTOM_LEFT;
+            imp->fmt.orientation = ORIENT_BOTTOM_LEFT;
             break;
         case ORIENT_TOP_RIGHT:
-            tc->fmt.orientation = ORIENT_BOTTOM_RIGHT;
+            imp->fmt.orientation = ORIENT_BOTTOM_RIGHT;
             break;
         case ORIENT_BOTTOM_LEFT:
-            tc->fmt.orientation = ORIENT_TOP_LEFT;
+            imp->fmt.orientation = ORIENT_TOP_LEFT;
             break;
         case ORIENT_BOTTOM_RIGHT:
-            tc->fmt.orientation = ORIENT_TOP_RIGHT;
+            imp->fmt.orientation = ORIENT_TOP_RIGHT;
             break;
         case ORIENT_LEFT_TOP:
-            tc->fmt.orientation = ORIENT_RIGHT_TOP;
+            imp->fmt.orientation = ORIENT_RIGHT_TOP;
             break;
         case ORIENT_LEFT_BOTTOM:
-            tc->fmt.orientation = ORIENT_RIGHT_BOTTOM;
+            imp->fmt.orientation = ORIENT_RIGHT_BOTTOM;
             break;
         case ORIENT_RIGHT_TOP:
-            tc->fmt.orientation = ORIENT_LEFT_TOP;
+            imp->fmt.orientation = ORIENT_LEFT_TOP;
             break;
         case ORIENT_RIGHT_BOTTOM:
-            tc->fmt.orientation = ORIENT_LEFT_BOTTOM;
+            imp->fmt.orientation = ORIENT_LEFT_BOTTOM;
             break;
     }
 
