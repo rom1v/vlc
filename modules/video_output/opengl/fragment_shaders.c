181f4a48fca8e9bc6095f36493abb277b9905cd3
Romain Vimont <rom1v@videolabs.io>
2020-02-04 11:41:48 +0100

    opengl: apply orientation in fragment shader
    Apply all texture coordinates transformation in the fragment shader.
    
    This is necessary to be able to implement a renderer independent of the
    input picture (i.e. without even knowing the details) by using a single
    GLSL function "vlc_texture(vec2 pic_coords)".

--- modules/video_output/opengl/fragment_shaders.c
+++ modules/video_output/opengl/fragment_shaders.c
@@ -492,7 +492,9 @@ opengl_fragment_shader_init(struct vlc_gl_renderer *renderer, GLenum tex_target,
 
     ADDF("%s", renderer->glsl_precision_header);
 
-    ADD("varying vec2 PicCoords;\n");
+    ADD("varying vec2 PicCoords;\n"
+        "uniform mat4 TransformMatrix;\n"
+        "uniform mat4 OrientationMatrix;\n");
     for (unsigned i = 0; i < interop->tex_count; ++i)
         ADDF("uniform %s Texture%u;\n"
              "uniform mat3 TexCoordsMap%u;\n", sampler, i, i);
@@ -588,8 +590,8 @@ opengl_fragment_shader_init(struct vlc_gl_renderer *renderer, GLenum tex_target,
 
     ADD("uniform vec4 FillColor;\n"
         "void main(void) {\n");
-    /* Homogeneous coordinates */
-    ADDF(" vec3 pic_hcoords = vec3(%s, 1.0);\n", coord_name);
+    /* Homogeneous (oriented) coordinates */
+    ADDF(" vec3 pic_hcoords = vec3((TransformMatrix * OrientationMatrix * vec4(%s, 0.0, 1.0)).st, 1.0);\n", coord_name);
     ADD(" vec2 tex_coords;\n");
 
     if (tex_target == GL_TEXTURE_RECTANGLE)
