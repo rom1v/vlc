d27f39aa4a29daa72d612002459ebd38ed045228
Romain Vimont <rom1v@videolabs.io>
2020-02-06 12:39:35 +0100

    opengl: do not pass the picture_t to the renderer
    The renderer is expected to be independant of the input picture format.
    Instead, update the sampler directly.

--- modules/video_output/opengl/vout_helper.c
+++ modules/video_output/opengl/vout_helper.c
@@ -263,7 +263,7 @@ int vout_display_opengl_Prepare(vout_display_opengl_t *vgl,
 {
     GL_ASSERT_NOERROR();
 
-    int ret = vlc_gl_renderer_Prepare(vgl->renderer, picture);
+    int ret = vlc_gl_sampler_Update(vgl->sampler, picture);
     if (ret != VLC_SUCCESS)
         return ret;
 
