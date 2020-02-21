fe4659d6188f61f0e48db94b2722ff54db2b3ad0
Romain Vimont <rom1v@videolabs.io>
2020-02-21 13:57:38 +0100

    WIP framebuffers

--- modules/video_output/opengl/filter_priv.h
+++ modules/video_output/opengl/filter_priv.h
@@ -32,6 +32,9 @@ struct vlc_gl_filter_priv {
     struct vlc_gl_tex_size size_out;
     struct vlc_gl_sampler *sampler; /* owned */
 
+    GLuint framebuffer_out; /* owned (this filter must delete it) */
+    GLuint texture_out; /* owned (attached to the framebuffer_out) */
+
     struct vlc_list node; /**< node of vlc_gl_filters.list */
 };
 
