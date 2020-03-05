f8e31b43a21fa114bddde869b93eb5b908ba1175
Romain Vimont <rom1v@videolabs.io>
2020-03-05 16:12:50 +0100

    opengl: pass picture PTS to filters
    The filters may depend on the PTS. Pass a metadata structure to their
    draw() callback.
    
    Co-authored-by: Alexandre Janniaux <ajanni@videolabs.io>

--- modules/video_output/opengl/filters.c
+++ modules/video_output/opengl/filters.c
@@ -41,6 +41,7 @@ vlc_gl_filters_Init(struct vlc_gl_filters *filters, struct vlc_gl_t *gl,
     filters->interop = interop;
     vlc_list_init(&filters->list);
     memset(&filters->viewport, 0, sizeof(filters->viewport));
+    filters->pts = 0;
 
     GLint value;
 
@@ -227,6 +228,8 @@ vlc_gl_filters_UpdatePicture(struct vlc_gl_filters *filters,
 
     assert(first_filter);
 
+    filters->pts = picture->date;
+
     return vlc_gl_sampler_UpdatePicture(first_filter->sampler, picture);
 }
 
@@ -234,6 +237,10 @@ int
 vlc_gl_filters_Draw(struct vlc_gl_filters *filters)
 {
     const opengl_vtable_t *vt = &filters->api->vt;
+
+    struct vlc_gl_input_meta meta = {
+        .pts = filters->pts,
+    };
 
     struct vlc_gl_filter_priv *priv;
     vlc_list_foreach(priv, &filters->list, node)
@@ -273,7 +280,7 @@ vlc_gl_filters_Draw(struct vlc_gl_filters *filters)
             vt->Viewport(0, 0, priv->size_out.width, priv->size_out.height);
 
         struct vlc_gl_filter *filter = &priv->filter;
-        int ret = filter->ops->draw(filter);
+        int ret = filter->ops->draw(filter, &meta);
         if (ret != VLC_SUCCESS)
             return ret;
 
@@ -286,7 +293,7 @@ vlc_gl_filters_Draw(struct vlc_gl_filters *filters)
             vt->BindFramebuffer(GL_DRAW_FRAMEBUFFER, draw_fb);
 
             struct vlc_gl_filter *subfilter = &subfilter_priv->filter;
-            ret = subfilter->ops->draw(subfilter);
+            ret = subfilter->ops->draw(subfilter, &meta);
             if (ret != VLC_SUCCESS)
                 return ret;
         }
