549d0ce16b3343584c8cee49944815ded6f4b723
Romain Vimont <rom1v@videolabs.io>
2020-03-05 16:12:50 +0100

    opengl: pass picture PTS to filters
    The filters may depend on the PTS. Pass a metadata structure to their
    draw() callback.
    
    Co-authored-by: Alexandre Janniaux <ajanni@videolabs.io>

--- modules/video_output/opengl/filters.c
+++ modules/video_output/opengl/filters.c
@@ -50,6 +50,9 @@ struct vlc_gl_filters {
         unsigned width;
         unsigned height;
     } viewport;
+
+    /** Last updated picture PTS */
+    vlc_tick_t pts;
 };
 
 struct vlc_gl_filters *
@@ -66,6 +69,7 @@ vlc_gl_filters_New(struct vlc_gl_t *gl, const struct vlc_gl_api *api,
     vlc_list_init(&filters->list);
 
     memset(&filters->viewport, 0, sizeof(filters->viewport));
+    filters->pts = 0;
 
     return filters;
 }
@@ -263,6 +267,8 @@ vlc_gl_filters_UpdatePicture(struct vlc_gl_filters *filters,
 
     assert(first_filter);
 
+    filters->pts = picture->date;
+
     return vlc_gl_sampler_UpdatePicture(first_filter->sampler, picture);
 }
 
@@ -274,6 +280,10 @@ vlc_gl_filters_Draw(struct vlc_gl_filters *filters)
     GLint value;
     vt->GetIntegerv(GL_DRAW_FRAMEBUFFER_BINDING, &value);
     GLuint draw_framebuffer = value; /* as GLuint */
+
+    struct vlc_gl_input_meta meta = {
+        .pts = filters->pts,
+    };
 
     struct vlc_gl_filter_priv *priv;
     vlc_list_foreach(priv, &filters->list, node)
@@ -310,7 +320,7 @@ vlc_gl_filters_Draw(struct vlc_gl_filters *filters)
             vt->Viewport(0, 0, priv->size_out.width, priv->size_out.height);
 
         struct vlc_gl_filter *filter = &priv->filter;
-        int ret = filter->ops->draw(filter);
+        int ret = filter->ops->draw(filter, &meta);
         if (ret != VLC_SUCCESS)
             return ret;
 
@@ -323,7 +333,7 @@ vlc_gl_filters_Draw(struct vlc_gl_filters *filters)
             vt->BindFramebuffer(GL_DRAW_FRAMEBUFFER, draw_fb);
 
             struct vlc_gl_filter *subfilter = &subfilter_priv->filter;
-            ret = subfilter->ops->draw(subfilter);
+            ret = subfilter->ops->draw(subfilter, &meta);
             if (ret != VLC_SUCCESS)
                 return ret;
         }
